363462043fa22c83851561c6b36d3e7ffcc3f67d
elenaBushueva <helenabushueva@gmail.com>
2021-10-06 23:54:22 -0700

    dispatching actions, new reduser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {connect} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+    return (
+        <div>
+            Version : {props.appVersion}
+            <hr/>
 
-export default App;
+            Count : {props.myCount}
+            <button onClick={props.plus}> Plus </button>
+            <button onClick={props.minus}> Minus </button>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    myCount: state.count,
+    appVersion: state.appVersion
+});
+const mapDispatchToProps = dispatch => ({
+    plus: () => dispatch({type: "PLUS"}),
+    minus: () => dispatch({type: 'MINUS'})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+const redusers = (state, action) => {
+    console.log(state, action)
+
+    if(action.type === 'PLUS'){
+        return {
+            ...state,
+            count: state.count + 1
+        }
+    }
+
+    if(action.type === 'MINUS'){
+        return {
+            ...state,
+            count: state.count - 1
+        }
+    }
+
+    return {
+        count: 2,
+        appVersion: '0.0.1'
+    }
+}
+
+
+const store = createStore(redusers);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
